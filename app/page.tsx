015af1f6b43d7f9ea24aa6db9a27b8c999b550ea
Krystian Postek <krystian@postek.eu>
2023-05-23 13:38:42 +0200

    feat: idk whatimadoinghere

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
   return (
     <main className="h-screen">
       <div className="grid place-items-center h-full">
-        <div className="grid grid-cols-[1fr_4fr] grid-rows-[3fr_1fr] place-items-center place-content-center text-slate-950 h-[calc(90px_*_4_+_4rem)] w-[calc(90px_*_5_+_4rem)] gap-4">
-          <div className="bg-white p-8 w-full h-full rounded-xl overflow-clip grid gap-4 place-content-center">
+        <div className="grid md:grid-cols-[1fr_4fr] md:grid-rows-[3fr_1fr] grid-cols-1 grid-rows-[1fr_3fr_1fr] place-items-center place-content-center text-slate-950 h-[calc(90px_*_4_+_4rem)] md:w-[calc(90px_*_5_+_4rem)] w-4/5 gap-4">
+          <div className="bg-white p-8 w-full h-full rounded-xl overflow-clip grid gap-4 md:grid-cols-1 md:grid-flow-row grid-flow-col place-content-center">
             {/* contact buttons */}
             <ContactButton
               icon={<FaGithub className="mx-auto" />}
@@ -40,16 +40,14 @@ export default function Home() {
           <div className="bg-white p-8 w-full h-full rounded-xl">
             <h2 className="font-semibold text-3xl">Krystian Postek</h2>
             <p className="my-4">GraphQL enjoyer, react enthusiast (for some odd reason).</p>
-            <p className="my-4">Wszystkie moje projekty są na Github lub objęte NDA.</p>
+            <p className="my-4">Wszystkie moje projekty są na Github, w CV lub są objęte NDA.</p>
             <p className="my-4">Informacje o moim wykształceniu oraz doświadczeniu znajdziesz na LinkedIn lub w CV.</p>
           </div>
-          <div />
-          {/* spacer */}
-          <div className="bg-white p-8 w-full h-full rounded-xl flex items-center">
+          <div className="bg-white p-8 w-full h-full rounded-xl flex items-center md:col-span-2">
             <Link href={"/cv"}>
-              <div className="flex items-center gap-2 hover:gap-6 duration-100 ease-in-out text-xl">
-                <span className="underline underline-offset-4">curriculum vitae</span>
-                <HiArrowRight />
+              <div className="flex items-center gap-2 text-xl group hover:text-sky-600 duration-100 ease-in-out">
+                <span>curriculum vitae</span>
+                <HiArrowRight className="group-hover:rotate-180 group-hover:ml-4 duration-200 ease-in-out" />
               </div>
             </Link>
           </div>
